861c037d0267cc5a9ba7cd441bd1165c3e1558b5
anilgurjar <anilgurjer371@gmail.com>
2017-10-29 16:44:44 +0530

    b

--- src/Controller/Api/CompaniesController.php
+++ src/Controller/Api/CompaniesController.php
@@ -112,7 +112,7 @@ class CompaniesController extends AppController
 		{   
 			$organisation_name=$this->request->data['company_organisation'];
 			$find_id_Companies=$this->Companies->find('list')->where(['company_organisation'=>$organisation_name]);
-			pr($find_id_Companies); exit;
+			pr($find_id_Companies->toArray()); exit;
 			$result_Companies=$this->Companies->find()->select(['form_number'])->order(['form_number' => 'DESC'])->first();
 			 $form_number=$result_Companies->form_number+1;
 			$this->request->data['year_of_joining']=date("Y-m-d");
