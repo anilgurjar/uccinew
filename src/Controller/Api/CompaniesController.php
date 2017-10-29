47170817263c9ce88b081f9563402d73970bd12a
anilgurjar <anilgurjer371@gmail.com>
2017-10-29 17:06:08 +0530

    mbjh

--- src/Controller/Api/CompaniesController.php
+++ src/Controller/Api/CompaniesController.php
@@ -111,9 +111,9 @@ class CompaniesController extends AppController
 		if($this->request->is(['post','put']))
 		{   
 			$organisation_name=$this->request->data['company_organisation'];
-			$find_id_Companies=$this->Companies->find('list')->where(['company_organisation LIKE'=>$organisation_name]);
-			$find_id_CoRegistration=$this->Companies->CoRegistrations->find('list')->where(['company_id'=>$find_id_Companies]);
-			pr($find_id_Companies->toArray()); exit;
+			$find_id_Companies=$this->Companies->find()->where(['company_organisation LIKE'=>$organisation_name]);
+			$find_id_CoRegistration=$this->Companies->CoRegistrations->find()->where(['company_id'=>$find_id_Companies->id]);
+			pr($find_id_CoRegistration->toArray()); exit;
 			$result_Companies=$this->Companies->find()->select(['form_number'])->order(['form_number' => 'DESC'])->first();
 			 $form_number=$result_Companies->form_number+1;
 			$this->request->data['year_of_joining']=date("Y-m-d");
