924a7af0653d4f0c41c6940a43959ddf4ffa2ee2
anilgurjar <anilgurjer371@gmail.com>
2017-10-30 15:47:02 +0530

    ggg

--- src/Controller/Api/CompaniesController.php
+++ src/Controller/Api/CompaniesController.php
@@ -157,7 +157,7 @@ class CompaniesController extends AppController
 						->where(['co_registration_id' => $find_id_CoRegistration_id])
 						->execute();	
 				}else{
-					/* $query = $this->Companies->CoRegistrations->query();
+					$query = $this->Companies->CoRegistrations->query();
 					$query->insert(['amount', 'tax_amount','total_amount','master_financial_year_id'])
 						->values([
 							'amount' => $amount,
@@ -174,7 +174,7 @@ class CompaniesController extends AppController
 							'tax_percentage' => $tax_percentage,
 							'amount' => $amount,
 						])
-						->execute();  */
+						->execute(); 
 				
 				}
 				}	
