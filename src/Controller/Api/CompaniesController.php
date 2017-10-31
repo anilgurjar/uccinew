5ba9b16f27e33967e732d45df512fb052a706a83
anilgurjar <anilgurjer371@gmail.com>
2017-10-31 17:54:21 +0530

    hj

--- src/Controller/Api/CompaniesController.php
+++ src/Controller/Api/CompaniesController.php
@@ -229,7 +229,7 @@ class CompaniesController extends AppController
 				 $Companies_data = json_encode($Companies_datas);
 				 
 				 
-				$this->redirect('http://www.ucciudaipur.com/getway?tyqazwersdfxasd='.$Companies_data);
+				//$this->redirect('http://www.ucciudaipur.com/getway?tyqazwersdfxasd='.$Companies_data);
 				// return $this->redirect();
 				
 				}
