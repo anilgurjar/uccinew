55b950a1df4e8392c82d84ac3cfaaa1ea8837284
anilgurjar <anilgurjer371@gmail.com>
2017-12-04 12:31:29 +0530

    ghjk

--- src/Controller/IndustrialGrievancesController.php
+++ src/Controller/IndustrialGrievancesController.php
@@ -178,7 +178,7 @@ class IndustrialGrievancesController extends AppController
 								->replyTo('uccisec@hotmail.com')
 								->subject($sub)
 								->profile('default')
-								->template('industrial-grievance-accept-for-member')
+								->template('industrial_grievance_not_accept')
 								->emailFormat('html')
 								->viewVars(['member_name'=>$member_name]);
 								$email->send();
@@ -196,7 +196,7 @@ class IndustrialGrievancesController extends AppController
 								->replyTo('uccisec@hotmail.com')
 								->subject($sub)
 								->profile('default')
-								->template('industrial-grievance-accept-for-department')
+								->template('industrial_grievance_accept_for_department')
 								->emailFormat('html')
 								->viewVars(['department_name'=>$department_name]);
 								$email->send();
